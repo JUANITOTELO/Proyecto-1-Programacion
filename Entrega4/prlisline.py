693147be8c07f4a294aa53621990320c08ff2967
JUANITOTELO <44297625+JUANITOTELO@users.noreply.github.com>
2019-05-15 18:15:29 -0500

    ACTUALIZACIÓN

--- Entrega4/prlisline.py
+++ Entrega4/prlisline.py
@@ -9,6 +9,9 @@ from pygame.locals import *
 x = 10
 y = 10
 
+def dibujarLineas(v,c,i,f,a,lista):
+    pygame.draw.line(v,c,i,f,a)
+
 def main():
     global x
     global y
@@ -17,51 +20,57 @@ def main():
     pygame.display.set_caption("BatCar!!")
     FPS = 500
     clock = pygame.time.Clock()
-    l = []
+    pInicio = [x,y]
     c = 10
     c2 = 0
     x = 10
     y = 10
     xf = 10
     yf = 10
+    l = [pInicio,(xf,yf)]
 
     while True:
         ventana.fill((255,255,255))
         random_color = (randint(100,255), randint(0,199), randint(0,200))
-        pInicio = [x,y]
         tkla = pygame.key.get_pressed() 
+        
         oldxf = xf
         oldyf = yf
+        
         if tkla[K_LEFT]:
-            xf -= 5
+            xf -= 2
             l.append((xf,yf))
             print(l)
             print(l[c2])
             c2 += 1
+
             
         if tkla[K_RIGHT]: 
-            xf += 5
+            xf += 2
             l.append((xf,yf))
             print(l)
             print(l[c2])
             c2 += 1
+
             
         if tkla[K_DOWN]:
-            yf += 5 
+            yf += 2 
             l.append((xf,yf))
             print(l)
             print(l[c2])
             c2 += 1
             
         if tkla[K_UP]:
-            yf -= 5
+            yf -= 2
             l.append((xf,yf))
             print(l)
             print(l[c2])
             c2 += 1
-            
-        pygame.draw.line(ventana,random_color,pInicio,(xf,yf),7)
-		pygame.draw.line()
+
+        
+        dibujarLineas(ventana,random_color,l[c2],l[c2+1],7,l)
+        
+        
         for evento in pygame.event.get():
             if evento.type == QUIT:
                 pygame.quit()
