c799ac5523157ef7ffa06b73c90c7adecfb6f383
JUANITOTELO <44297625+JUANITOTELO@users.noreply.github.com>
2019-05-19 18:46:21 -0500

    Add files via upload

--- BatCar/ProyectoFinal/controles.ino
+++ BatCar/ProyectoFinal/controles.ino
@@ -7,8 +7,14 @@ int derA = 9;
 int derB = 10; 
 int vel = 255;            // Velocidad de los motores (0-255)
 int estado = 'c';
+int pecho = 2;           
+int ptrig = 3;            
+const int TRIG = 3;   //Pin digital 3 para el Trigger del sensor
+const int ECHO = 2;   //Pin digital 2 para el Echo del sensor  
 void setup() {
-  Serial.begin(9600);    // inicia el puerto serial para comunicacion con el Bluetooth
+  Serial.begin(9600);
+  pinMode(ECHO, INPUT);    // inicia el puerto serial para comunicacion con el Bluetooth
+  pinMode(TRIG, OUTPUT);
   pinMode(derA, OUTPUT);
   pinMode(derB, OUTPUT);
   pinMode(izqA, OUTPUT);
@@ -17,8 +23,22 @@ void setup() {
 
 }
 
-void loop() {
-   if(Serial.available()>0){        // lee el bluetooth y almacena en estado
+void loop() 
+{
+  long duracion, distancia; 
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(4);          //Enviamos un pulso de 10us
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  
+  duracion = pulseIn(ECHO, HIGH); //obtenemos el ancho del pulso
+  distancia = (duracion/2)/29;             //escalamos el tiempo a una distancia en cm
+  Serial.println(distancia);      //Enviamos serialmente el valor de la distancia
+  delay(50);          //Hacemos una pausa de 100ms 
+  
+  
+  if(Serial.available()>0){        // lee el bluetooth y almacena en estado
     estado = Serial.read();
   }
   if(estado=='a'){           // Boton desplazar al Frente
